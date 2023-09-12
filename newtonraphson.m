e657028f9225139955c1c254b08497ef267c9381
felipeversiane <111650483+felipeversiane@users.noreply.github.com>
2023-09-12 18:38:52 -0300

    final ex2

--- newtonraphson.m
+++ newtonraphson.m
@@ -1,12 +1,21 @@
 function newtonraphson()
-  maxInt = 100;
+  clc
+  maxInt = 1000;
   x0 = [0.5;1;0];
   tol = 1e-5;
+  [allX,allFx,cont,x1] = structure(maxInt,tol,x0);
+  fprintf("Levou %i iteracoes para achar a raiz \n",cont);
+  fprintf("A raiz de x :%5.5f \n",x1(1));
+  fprintf("A raiz de y :%5.5f \n",x1(2));
+  fprintf("A raiz de z :%5.5f \n",x1(3));
+  plotCon(allX,allFx,cont);
+endfunction
+
+function [allX,allFx,cont,x1] = structure(maxInt,tol,x0)
   allX = zeros(maxInt,3);
   allFx= zeros(maxInt,3);
-  
   for cont = 1:maxInt
-    allFx = sistem(x0);
+    allFx(cont, :) = sistem(x0);
     x1 = x0 - inv(jacob(x0))*sistem(x0);
     allX(cont, : ) = x1;
     if max(abs(x1-x0)) <= tol
@@ -18,14 +27,9 @@ function newtonraphson()
   endfor
   allX = allX(1:cont, :);
   allFx = allFx(1:cont, :);
-  fprintf("Levou %i iteracoes para achar a raiz \n",cont);
-  fprintf("A raiz de x1 :%5.5f \n",x1(1));
-  fprintf("A raiz de x2 :%5.5f \n",x1(2));
-  fprintf("A raiz de x3 :%5.5f \n",x1(3));
-  plotCon(allX,allFx,cont);
 endfunction
 
-function s = sistem(x0) 
+function s = sistem(x0)
   x = x0(1);
   y = x0(2);
   z = x0(3);
@@ -39,15 +43,16 @@ function j = jacob(x0)
   y = x0(2);
   z = x0(3);
   j = [6,-2,exp(z);
-  cos(x),-1,1;
-  cos(x),2,3]; 
+      cos(x),-1,1;
+      cos(x),2,3];
 endfunction
 
 function plotCon(allX,allFx,cont)
-   figure(2);
+   figure(1);
 
    xPlot = allX(1:cont, :);
    fxPlot = allFx(1:cont, :);
+
    subplot(2,1,1);
    plot(1:cont,[xPlot(:, 1)],'color','k','linewidth',2);
    hold on
@@ -56,12 +61,12 @@ function plotCon(allX,allFx,cont)
    hold off
    title("Grafico de convergencia da relacao x1");
    xlabel("Iteracoes");
-   ylabel("x1");
+   ylabel("(x,y,z)");
+   legend("x","y","z");
    set(gca,'fontsize',12);
    grid on;
 
 
-   keyboard
    subplot(2,1,2)
    plot(1:cont,[fxPlot(:,1)],'color','k','linewidth',2);
    hold on
@@ -70,7 +75,8 @@ function plotCon(allX,allFx,cont)
    hold off
    title("Grafico de convergencia da relacao f(x1)");
    xlabel("Iteracoes");
-   ylabel("f(x1)");
+   ylabel("f(x,y,z)");
+   legend("x","y","z");
    set(gca,'fontsize',12);
    grid on;
 endfunction
