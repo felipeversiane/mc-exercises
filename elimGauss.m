b614d44ad3042407a101af56308ead05319a83c6
felipeversiane <111650483+felipeversiane@users.noreply.github.com>
2023-09-20 18:28:25 -0300

    resolving bugs

--- elimGauss.m
+++ elimGauss.m
@@ -1,14 +1,12 @@
  function elimGauss()
   clc
-  A = [20 5 -8 3 3;5 -3/2 4 1 3;-14 9 9 3 1; -3 10 20 30 20; -3 10 10 30 2];
-  b = [32; -4; 3; 30; 10];
+  A = [0.780 0.563;0.913 0.659];
+  b = [0.217;0.254];
   [Aa] = pivoting(A,b);
   [Aa] = zeroing(Aa);
   [Aa,x1] = structure(Aa);
-  Aa
   x1
  endfunction
-
 
  function [Aa] = pivoting(A,b)
 
@@ -30,14 +28,30 @@
   for i=1:n-1
     pivot = Aa(i,i);
     for j=i+1:n
-      f = Aa(j,i)/pivot;
-      Aa(j,:) = Aa(j,:) - f*Aa(i,:);
+      if j <= n
+        fator = Aa(j,i)/pivot;
+        Aa(j,i) = 0;
+      end
     end
   end
 
-endfunction
+  endfunction
 
-function [Aa,x1] = structure(Aa)
-  x1 = zeros(size(Aa,1),1);
-endfunction
+  function [Aa,x1] = structure(Aa)
+    n = size(Aa,1);
+    x1 = zeros(n,1);
+    x1(n) = Aa(n,n);
+    for i = n-1:-1:1
+      soma = 0;
+      for j = i+1:n
+        soma = soma + Aa(i,j) * x1(j);
+      end
+      x1(i) = Aa(i,n) - soma;
+    end
+   endfunction
+
+  function s = sistem(x1,x2)
+    s = [0.780.*x1 + 0.563.*x2 - 0.217;
+         0.913.*x1 + 0.659.*x2 - 0.254];
+  endfunction
 
