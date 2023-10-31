06a1eed641879412410594558b30310a41b7e933
felipeversiane <111650483+felipeversiane@users.noreply.github.com>
2023-10-31 08:56:01 -0300

    Update otimizacao.m

--- otimizacao.m
+++ otimizacao.m
@@ -4,38 +4,49 @@ function otimizacao()
   tol = 1e-3;
   x0 = [4;
         1];
-  is = 2;
+  is = 1;
   if is == 1
-    a = 3;
-    [x1,cont] = gradient(maxIter,tol,x0,a);
+    a = 0.1;
+    [x1,cont,allX,allFx] = gradient(maxIter,tol,x0,a);
   elseif is == 2
-    a = 0.10;
-    [x1,cont] = newton(maxIter,tol,x0,a);
+    a = 0.9;
+    [x1,cont,allX,allFx] = newton(maxIter,tol,x0,a);
   endif
   x1
   cont
+  plotAll(cont,allX,allFx);
 endfunction
 
-function [x1,cont] = gradient(maxIter,tol,x0,a)
+function [x1,cont,allX,allFx] = gradient(maxIter,tol,x0,a)
+  allX = zeros(maxIter,2);
+  allFx = zeros(maxIter,2);
   for cont = 1:maxIter
        x1 = x0 - a*g(x0);
-    if (abs(max(x1-x0)) <= tol)
-      x0 = x1;
+       allX(cont,:) = x1;
+       allFx(cont,:) = f(x1);
+    if (max(abs(x1-x0))<= tol)
       break
     endif
     x0 = x1;
   endfor
 endfunction
 
-function [x1,cont] = newton(maxIter,tol,x0,a)
+function [x1,cont,allX,allFx] = newton(maxIter,tol,x0,a)
+  allX = zeros(maxIter,2);
+  allFx = zeros(maxIter,2);
   for cont = 1:maxIter
        x1 = x0 - a*inv(h(x0))*g(x0);
-    if (abs(max(x1-x0)) <= tol)
-      x0 = x1;
+       allX(cont,:) = x1;
+       allFx(cont,:) = f(x1);
+   if (max(abs(x1-x0))<= tol)
       break
     endif
     x0 = x1;
   endfor
+endfunction
+
+function plotAll(cont,allX,allFx)
+  
 endfunction
 
 
