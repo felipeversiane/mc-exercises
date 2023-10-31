e778ce2441aaa8988635fe85c22bd73f1f2f17d5
felipeversiane <111650483+felipeversiane@users.noreply.github.com>
2023-10-31 10:20:53 -0300

    Update otimizacao.m

--- otimizacao.m
+++ otimizacao.m
@@ -1,20 +1,26 @@
 function otimizacao()
   clc
   maxIter = 1000;
-  tol = 1e-3;
-  x0 = [4;
-        1];
+  tol = 1e-3
+  x0 = [4;1];
+  x = [3;0.5];
   is = 1;
   if is == 1
-    a = 0.1;
+    a = 0.02;
     [x1,cont,allX,allFx] = gradient(maxIter,tol,x0,a);
   elseif is == 2
-    a = 0.9;
+    a = 0.2222;
     [x1,cont,allX,allFx] = newton(maxIter,tol,x0,a);
   endif
   x1
   cont
+  [result] = distEuclidiana(x,x1);
+  result
   plotAll(cont,allX,allFx);
+endfunction
+
+function [result] = distEuclidiana(x,x1)
+  result = sqrt((x(1)-x1(1))^2+(x(2)-x1(2))^2);
 endfunction
 
 function [x1,cont,allX,allFx] = gradient(maxIter,tol,x0,a)
@@ -45,8 +51,39 @@ function [x1,cont,allX,allFx] = newton(maxIter,tol,x0,a)
   endfor
 endfunction
 
-function plotAll(cont,allX,allFx)
-  
+function plotAll(iter,allX,allFx)
+  x = 4:0.1:1;
+  y = x;
+  z = zeros(length(x), length(y), 2);
+  for xx = 1:numel(x)
+    for yy = 1:numel(y)
+      z(xx, yy, 1:2) = f([x(xx); y(yy)]);
+    end
+  end  
+   for cont = 1:iter
+    figure(1);
+    subplot(211)
+    surf(x, y, z(:, :, 1));
+    hold on
+    plot3(allX(1:cont, 2), allX(1:cont, 1), allFx(1:cont, 1), 'color', 'r', 'linewidth', 2)
+    plot3(allX(cont, 2), allX(cont, 1), allFx(cont, 1), 'o', 'markersize', 20, 'color', 'r', 'markerfacecolor', [1 1 1], 'linewidth', 2)
+    hold off
+    set(gca, 'fontsize', 12);
+    grid on;
+    title(sprintf('Iter %i', cont));
+    subplot(212)
+    surf(x, y, z(:, :, 2));
+    hold on;
+    plot3(allX(1:cont, 2), allX(1:cont, 1), allX(1:cont, 2), 'color', 'r', 'linewidth', 2)
+    plot3(allX(cont, 2), allX(cont, 1), allX(cont, 2), 'o', 'markersize', 20, 'color', 'r', 'markerfacecolor', [1 1 1], 'linewidth', 2)
+    hold off;
+    set(gca, 'fontsize', 12);
+    grid on;
+    pause(0.3);
+  endfor
+endfunction
+
+function plotConv(iter,allX,allFx)
 endfunction
 
 
