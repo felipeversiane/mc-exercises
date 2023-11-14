2f77e183a4711b13054fc87e12c84bd29f9a3bd0
felipeversiane <111650483+felipeversiane@users.noreply.github.com>
2023-11-14 08:58:41 -0300

    Add files via upload

---
+++ interpolacao.m
@@ -0,0 +1,14 @@
+function interpolacao()
+  clc
+  degree = 2;
+  x = [0;1;2;3;4;5];
+  y = [0;0.5;0.8;0.9;0.941176;0.961538];
+  xx = 4.7;
+  if(length(x)<degree+1)
+    error("Nao sera possivel continuar");
+  endif
+  
+endfunction
+
+function [a,b]=regressao()
+
