51ac70a3aabac4634ccee323cbc1a17cb198e2c7
felipeversiane <111650483+felipeversiane@users.noreply.github.com>
2023-11-21 10:50:24 -0300

    Update interpolacao.m

--- interpolacao.m
+++ interpolacao.m
@@ -1,4 +1,4 @@
-  function interpolacao()
+ function interpolacao()
     clc
     degree = 4;
     x = [0;1;2;3;4;5];
@@ -7,36 +7,29 @@
     if(numel(x)<degree+1)
       error("Nao sera possivel continuar");
     endif
-    [A,b,xVal] = regressao(degree,x,y);
+    xVal = regressao(degree,x,y);
+    yy = f(xVal,xx);
     [r2,r] = calcularR(x,y,xVal);
-    fprintf("O resultado do polinomio estimado para x=4.7 eh: %f.5 \n",f(xVal,xx));
+    fprintf("O resultado do polinomio estimado para x=4.7 eh: %f.5 \n",yy);
     fprintf("O resultado do r2 eh: %f.5 \n",r2);
     fprintf("O resultado do r eh: %f.5 \n",r);
-    plotGrafico(xVal,xx,x,y)
-    
+    plotGrafico(xVal,x,y,xx,yy);
   endfunction
 
-  function [A,b,xVal]=regressao(degree,x,y)
+  function xVal = regressao(degree,x,y)
+    m = zeros(degree+1,degree+2);
+    A = zeros(degree+1,degree+1);
+    b = zeros(degree+1,1);
     for i=1:degree+1
-      for j=1:i
-        k = i + j - 2;
-        soma = 0;
-        for l=1:numel(x)
-          soma = soma + x(l)^k;
-        endfor
-        a(i,j) = soma;
-        a(j,i) = soma;      
+      for j=1:degree+1
+        A(i,j) = sum(x.^(i+j-2));
       endfor
-      soma = 0;
-      for l=1:numel(x)
-        soma = soma + y(l)*x(l)^(i-1);
-      endfor
-      a(i,degree+2)= soma;
+      b(i) = sum(x.^(i-1).*y);
     endfor
-    A = a(:,1:end-1);
-    b = a(:,end);
-    xVal = A\b;
+    xVal = zeros(degree+1,1);
+    xVal = inv(A)*b;
   endfunction
+
 
   function [r2,r] = calcularR(x,y,xVal)
     sr = 0;
@@ -51,28 +44,31 @@
 
   function y = f(xVal,xx)
     y = 0;
-    for i=numel(xVal):-1:1
+    for i=length(xVal):-1:1
       y = y + xVal(i)*xx^(i-1);
     endfor
   endfunction
 
-  function plotGrafico(xVal, xx, x, y)
-    aux = x(1):0.1:x(end);
-    polF = zeros(size(aux));
+  function plotGrafico(xVal, x, y,xx,yy)
+    auxX = x(1):0.1:x(end);
+    auxY = zeros(size(auxX));
 
-    for i = 1:numel(aux)
-      polF(i) = f(xVal, aux(i));
+    for i = 1:numel(auxX)
+      auxY(i) = f(xVal, auxX(i));
     endfor
 
-    plot(aux, polF, 'color', 'k', 'linewidth', 2);
+    plot(auxX, auxY, 'color', 'k', 'linewidth', 2);
     hold on;
 
-    plot(x, y,'o', 'markersize', 20, 'color', 'r', 'markerfacecolor', 'r');
-    hold off;
+    plot(x, y,'o', 'markersize', 12, 'color', 'r', 'markerfacecolor', 'r');
+    hold on;
+    
+    plot(xx, yy,'o', 'markersize', 12, 'color',  [0 1 1], 'markerfacecolor', [0 1 1]);
+    hold off
 
     set(gca, 'fontsize', 12);
     title("Gráfico");
-    legend("Função do polinômio", "Pontos")
+    legend("Função do polinômio", "Dados","Ponto estimado");
     grid on;
     xlabel("x");
     ylabel("y");
